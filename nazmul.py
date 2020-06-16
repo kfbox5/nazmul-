b4fb1bf6e17a9440ca0f7d531ffe81655922bdf2
kfbox5 <67000044+kfbox5@users.noreply.github.com>
2020-06-16 18:41:19 +0600

    python2 nazmul.py

--- nazmul.py
+++ nazmul.py
@@ -59,7 +59,7 @@ logo = """ -----------------------------•◈•
 \033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
 \033[1;96mInstagram \033[1;93m: \033[1;FlowRana
 \033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/kfbox5/nazmul
 \033[1;91m======================================="""
 
 def tik():
